df28c43cca9fd023f8c0867b054839ec4ae9e2bc
Rishabh Gupta <3rishabh3@gmail.com>
2023-09-27 22:02:45 +0530

    imported essential packages and added loggers using morgan package

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+// regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// cookies and file middlewares
+app.use(cookieParser());
+app.use(fileUpload());
+
+// morgan middleware
+app.use(morgan("tiny"));
 
 // import all routes
 const home = require("./routes/home");
