0750291863c3d37f621f4db9b3ffcfa5224889b6
Rishabh Gupta <3rishabh3@gmail.com>
2023-10-03 02:47:57 +0530

    get one order functionality added

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,7 @@
 const BigPromise = require("../middlewares/bigPromise");
 const Order = require("../models/order");
 const Product = require("../models/product");
+const CustomError = require("../utils/customErrors");
 
 exports.createOrder = BigPromise(async (req, res, next) => {
   const {
@@ -21,6 +22,22 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     totalAmount,
     user: req.user._id,
   });
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getOneOrder = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new CustomError("Please check order id", 401));
+  }
 
   res.status(200).json({
     success: true,
