7e50702839ff17d8a43d653c904f0e2702e76066
Rishabh Gupta <3rishabh3@gmail.com>
2023-10-03 00:01:25 +0530

    created order schema

---
+++ routes/order.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const {} = require("../controllers/orderController");
+
+// router.route("/").get(home);
+
+module.exports = router;
